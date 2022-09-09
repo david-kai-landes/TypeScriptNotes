ee392cae801b0e9ebcb13660540a16c0d1b08d3e
David Landes <davidkailandes@gmail.com>
2022-09-09 16:53:49 -0400

    added more notes to Arrays.ts file

--- arrays.ts
+++ arrays.ts
@@ -4,3 +4,19 @@ const dates = [new Date(), new Date()];
 const carsByMake: string[][] = [];
 
 // Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+//Prevent incompatible values
+// carMakers.push(100);
+
+//Help with "MAP"
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
+
+//  Multiple Types in Arrays
+//  Flexible types
+const importantDates: (Date | string)[] = [];
+importantDates.push("2020-10-10"); //Example
+importantDates.push(new Date());
