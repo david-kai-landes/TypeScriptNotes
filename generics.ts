828e4a109cbd7e3a27f8097ec8dd01729d93b850
David Landes <davidkailandes@gmail.com>
2022-09-19 14:06:06 -0400

    added generics and constraints

--- generics.ts
+++ generics.ts
@@ -22,3 +22,52 @@ class ArrayOfAnithing<T> {
 }
 
 new ArrayOfAnithing<string>(["a", "b", "c"]);
+
+// Example of generics with functions
+
+function printString(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumber(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything(["a", "b", "c"]);
+
+// Example of Generic Constraints
+
+class Car {
+  print() {
+    console.log("I am a car");
+  }
+}
+
+class House {
+  print() {
+    console.log("I am a house");
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars<House>([new House(), new House()]);
+printHousesOrCars<Car>([new Car(), new Car()]);
